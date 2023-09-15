b509f427583f923a0ad5bb3b484c956b237eb4b0
Harrison25000 <ofcharrison@gmail.com>
2023-09-15 11:36:02 +0100

    getting user input from terminal

--- variables-exercise/src/Main.java
+++ variables-exercise/src/Main.java
@@ -1,17 +1,47 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//        String firstName = "Oliver";
+//        String lastName = "Harrison";
+//        String name = firstName + " " + lastName;
+//        System.out.println(name);
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+//        int num1 = 20;
+//        int num2 = 10;
+//
+//        int result =  num1 / num2;
+//        int num3 = 8;
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+//        System.out.println(num1 + num2);
+//        System.out.println(result);
+
+//        System.out.println(num3++);
+//        System.out.println(num3);
+//        System.out.println(++num3);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("First Name:");
+        String firstName = scanner.nextLine();
+        System.out.println("Last Name:");
+        String lastName = scanner.nextLine();
+
+        System.out.println("Hello " + firstName + " " + lastName);
+
+        System.out.println("Two numbers:");
+        int int1 = scanner.nextInt();
+        int int2 = scanner.nextInt();
+        int total = int1+int2;
+
+        System.out.println("Sum: " + total);
+
+
+
+
+
+
     }
 }
